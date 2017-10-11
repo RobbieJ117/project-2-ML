e84fa88ddaa4c5972c5b565566bd26c34f2cee95
Bryan Downs <xerobasix@gmail.com>
2017-10-11 11:15:06 -0600

    added my progress from earlier

---
+++ src/MLBP.py
@@ -0,0 +1,27 @@
+''' 
+MLBP.py is a Multi-layered Feed-forward Neural Network designed to approximate the
+Rosenbrock Equation
+
+@author Bryan Downs
+'''
+
+import sys
+import numpy as np
+
+
+
+
+
+'''
+mlbp is the main method. It takes 4 parameters.
+node_per_layer: the number of nodes per layer of the network
+num_h_layers: the number of hidden layers for the network
+...
+'''
+def mlbp():
+    pass
+
+
+
+
+print('something happens')
